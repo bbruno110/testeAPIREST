cea47cb44bc717de375c6bafd7414d49e5292ea6
bbruno110 <bruno.os230@gmail.com>
2022-06-02 21:48:56 -0300

    atualizacao

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,6 @@ const server =  express();
 server.use(cors())
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-server.use(express.static(path.join(__dirname, "/public")));
+//server.use(express.static(path.join(__dirname, "/public")));
 server.use(router);
 export default server;
