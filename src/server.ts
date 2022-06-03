249cffb74f4abd1e59838676b76134ad4e3d2fa1
bbruno110 <bruno.santos6@ftc.edu.br>
2022-06-03 10:42:47 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import app from './app';
 import 'dotenv/config';
 
 const port = process.env.PORT || 3000;
-
+app.get("/", function(req,res){
+    res.send("conectando");
+});
 
 app.listen(port, ()=> console.log(`app listeninig at ${process.env.PORT}`));
