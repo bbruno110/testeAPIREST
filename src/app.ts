d7f8c69742648ed15378bbd1fd0a471ded21e2c3
bbruno110 <bruno.santos6@ftc.edu.br>
2022-06-03 10:00:41 -0300

    teste

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const server =  express();
 server.use(cors());
 server.use(express.static(path.join(__dirname, "/public")));
 server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 server.use(router);
 server.use((req:Request, res:Response)=>{
     res.status(404);
